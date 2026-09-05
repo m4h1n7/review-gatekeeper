70437ed3f41f6f937fb7d2d40c54b40ddef06410
MAHIN HOSEN <276857492+m4h1n7@users.noreply.github.com>
2026-09-05 10:09:30 +0000

    Before: Fix and force-render the "Instant Client Demo Generator" component inside the Admin Portal:
    1. Navi...

--- isolate/assets/check-Cp6sB9LI.js
+++
@@ -1,1 +0,0 @@
-import{c}from"./index-BE2ym6LO.js";const e=[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]],t=c("check",e);export{t as C};
--- isolate/assets/chevron-right-DDw2fTSN.js
+++
@@ -1,1 +0,0 @@
-import{c as o}from"./index-BE2ym6LO.js";const t=[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]],e=o("chevron-right",t);export{e as C};
